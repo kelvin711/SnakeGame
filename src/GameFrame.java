d1dc6972297585387b15ced9243b51c3dfa9b244
Kelvin Arauz <arauzkelvin99@gmail.com>
2021-01-19 11:20:48 -0600

    first commit

---
+++ src/GameFrame.java
@@ -0,0 +1,25 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class GameFrame extends JFrame{
+	
+	GamePanel game;
+	JButton resetButton;
+	
+	GameFrame() {
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+
+	}
+
+
+}
